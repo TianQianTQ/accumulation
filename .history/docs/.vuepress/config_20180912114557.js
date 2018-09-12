6357c43b212a656d42971accda7d7834d3ce3028
tianqian <tianqian@360.net>
2018-09-12 15:22:45 +0800

    vue press编辑结构

--- .history/docs/.vuepress/config_20180912114557.js
+++ .history/docs/.vuepress/config_20180912114557.js
@@ -31,15 +31,11 @@ module.exports = {
       { text: 'GitHub', link:'https://github.com/TianQianTQ'},
       { text: '博客', link:'https://blog.csdn.net/t_tq_bnsg_bs_ll'}  
     ],
-    sidebar: 'auto',    // 自动匹配当前侧边栏---需后续重新配置
-    sidebarDepth: 2,   // 设置嵌套的标题链接深度\
-    displayAllHeaders: true, // 默认值：false  设置所有页面的标题链接
-    lastUpdated: 'Last Updated',   // 最后更新时间  --基于git
-    repo: 'TianQianTQ/accumulation',  // 自定义仓库地址
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为 "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
-  }, 
+    sidebar:{
+      
+    }
+  },
   markdown: { // 为每个代码块显示行号
     lineNumbers: true
-  },
+  }
 }
--- .history/docs/.vuepress/config_20180912145825.js
+++ .history/docs/.vuepress/config_20180912145825.js
@@ -35,9 +35,20 @@ module.exports = {
     sidebarDepth: 2,   // 设置嵌套的标题链接深度\
     displayAllHeaders: true, // 默认值：false  设置所有页面的标题链接
     lastUpdated: 'Last Updated',   // 最后更新时间  --基于git
-    repo: 'TianQianTQ/accumulation',  // 自定义仓库地址
+    repo: 'github.com/TianQianTQ',  // 自定义仓库地址
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为 "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: '查看源码',
+    // 以下为可选的编辑链接选项
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    // docsRepo: 'vuejs/vuepress',
+    // 假如文档不是放在仓库的根目录下：
+    // docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    // docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+     editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！'
   }, 
   markdown: { // 为每个代码块显示行号
     lineNumbers: true
